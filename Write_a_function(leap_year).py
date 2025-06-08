e6367b6de2976b41c4e37def96ac83cb0edb6bcf
doro26 <techananya123@gmail.com>
2025-06-08 20:53:57 +0530

    Write_a_function(leap_year).py

--- Write_a_function(leap_year).py
+++ Write_a_function(leap_year).py
@@ -1,3 +1,10 @@
+# This function determines whether a given year is a leap year or not.
+# It follows the leap year rules:
+# - Year divisible by 4 is a leap year,
+# - Except if it is divisible by 100, then it must also be divisible by 400 to be a leap year.
+# The function returns True if the year is leap, otherwise False.
+# A simple way to practice conditional statements and logical operators in Python.
+
 def is_leap(year):
     leap = False
     
