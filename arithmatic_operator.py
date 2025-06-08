1b5fc6da3149ffc6c85f82e22d798d43e033b8ef
doro26 <techananya123@gmail.com>
2025-06-08 20:54:49 +0530

    arithmatic_operator.py

--- arithmatic_operator.py
+++ arithmatic_operator.py
@@ -1,3 +1,11 @@
+
+# This function checks if a given year is a leap year or not.
+# A leap year is:
+# - Divisible by 4, AND
+# - Not divisible by 100 unless also divisible by 400.
+# Returns True if leap year, otherwise False.
+# Great for practicing conditionals and boolean logic in Python.
+
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
